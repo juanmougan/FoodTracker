eb485c55ff1631aa90be844c33feae3d44630fd0
Juan Manuel Mougan <juanmougan@gmail.com>
2016-07-03 23:43:50 -0300

    Initial structure

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Juan Manuel Mougan on 3/7/16.
+//  Copyright © 2016 juanmougan.github.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
